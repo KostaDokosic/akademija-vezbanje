fc7ae6f53af1f50247b1b9457def033662095814
Kosta <kosta99dt@gmail.com>
2024-08-02 20:22:25 +0200

    cleanstart

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize-typescript";
-import User from "../models/user.model";
 
 export default async function connectDatabase() {
   try {
@@ -13,7 +12,7 @@ export default async function connectDatabase() {
       logging: false,
     });
 
-    database.addModels([User]);
+    database.addModels([]);
     await database.sync();
 
     console.log("Database connected successfully");
--- src/types/express.types.ts
+++
@@ -1,4 +0,0 @@
-import { Request } from "express";
-import { IUser } from "./user.types";
-
-export type AuthRequest = Request & { user?: IUser };
